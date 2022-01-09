adec10ee8beea5c08220c8c3042b0775ebcf67e2
Jack DeFeo <jackd3243@gmail.com>
2022-01-09 12:46:51 -0500

    working on delete functionality

--- j-m-todo-front/src/components/Todo.js
+++ j-m-todo-front/src/components/Todo.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-function Todo(todos) {
+function Todo(todos, todo , setTodos) {
 
   const doSomething = e => {
     e.preventDefault();
@@ -9,7 +9,30 @@ function Todo(todos) {
       console.log("hi")
      // do something here
     }
-
+    const handleDelete = () => {
+      const revisedTodos = todos.filter(todo => todo.id ==! todo.id)
+      setTodos(revisedTodos)
+      const config = {
+        method: "DELETE",  
+        "Accepts": "application/json"
+      }
+      fetch(`http://localhost:9292/todo/${todo.id}`, config)
+    }
+  
+    // const onDelete = () => {
+    //   handleDelete(todos.id)
+    //   const config = {
+    //     method: "DELETE",  
+    //     "Accepts": "application/json"
+    //   }
+    //   fetch(`http://localhost:9292/todo/${todo.id}`, config)
+    // }
+    // const deleteTodos = (todo) => {
+    //   fetch(`http://localhost:9292/todo/${todo.id}`, {method: "DELETE"})
+  
+    //   const revisedTodos = todos.filter(todo => todo.id ==! todo.id)
+    //   setTodos(revisedTodos)
+    // }
 
 
   
@@ -26,7 +49,7 @@ function Todo(todos) {
           <button onClick={doSomething} type="button" className="btn" >
             Edit <span className="visually-hidden">{todos.name}</span>
           </button>
-        <button onClick={doSomething} type="button" className="btn btn__danger" >
+        <button onClick={handleDelete} type="button" className="btn btn__danger" >
             Delete <span className="visually-hidden">{todos.name}</span>
           </button>
         </div>
