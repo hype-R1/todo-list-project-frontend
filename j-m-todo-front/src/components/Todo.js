913d1fe24d931fb9413268d23e98fbf6a49ac8ef
Mike <mjpj@live.com>
2022-01-09 14:44:53 -0500

    mp

--- j-m-todo-front/src/components/Todo.js
+++ j-m-todo-front/src/components/Todo.js
@@ -2,18 +2,6 @@ import React from "react";
 
 
 function Todo(todos) {
-
-  const doSomething = e => {
-    e.preventDefault();
-
-      console.log("hi")
-     // do something here
-    }
-
-
-
-  
-
     return (
       <li className="todo stack-small">
        <div className="c-cb">
@@ -23,17 +11,16 @@ function Todo(todos) {
            </label>
         </div>
         <div className="btn-group">
-          <button onClick={doSomething} type="button" className="btn" >
+          <button type="button" className="btn">
             Edit <span className="visually-hidden">{todos.name}</span>
           </button>
-        <button onClick={doSomething} type="button" className="btn btn__danger" >
+          <button type="button" className="btn btn__danger">
             Delete <span className="visually-hidden">{todos.name}</span>
           </button>
         </div>
       </li>
     );
   }
-
 
 
   export default Todo;
